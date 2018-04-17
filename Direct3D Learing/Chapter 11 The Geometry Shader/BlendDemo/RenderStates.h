5d5c0472789f08dee7cffa0f87dfdf0fc118ef2a
haiaimi <wanghai1997@outlook.com>
2018-04-17 23:58:13 +0800

    几何着色器

---
+++ Direct3D Learing/Chapter 11 The Geometry Shader/BlendDemo/RenderStates.h
@@ -0,0 +1,29 @@
+//***************************************************************************************
+// RenderStates.h by Frank Luna (C) 2011 All Rights Reserved.
+//   
+// Defines render state objects.  
+//***************************************************************************************
+
+#ifndef RENDERSTATES_H
+#define RENDERSTATES_H
+
+#include "d3dUtil.h"
+
+class RenderStates
+{
+public:
+	static void InitAll(ID3D11Device* device);
+	static void DestroyAll();
+
+	static ID3D11RasterizerState* WireframeRS;
+	static ID3D11RasterizerState* NoCullRS;
+	 
+	static ID3D11BlendState* AlphaToCoverageBS;
+	static ID3D11BlendState* TransparentBS;
+	static ID3D11BlendState* BlotBS;
+
+	static ID3D11DepthStencilState* BlotDSS;
+	static ID3D11DepthStencilState* GlobalDDS;
+};
+
+#endif // RENDERSTATES_H
